073ab9bfccfa1219c8d008869cbb4cb53222c198
Deepanjali Singh <113447060+DEEPANJALI-11@users.noreply.github.com>
2025-09-22 14:12:55 +0000

    new project

---
+++ 01_viteBasic/src/App.jsx
@@ -0,0 +1,11 @@
+import Chai from './Chai'
+
+function App(){
+  return (
+    <>
+    <h1>You are under the app</h1>
+    <Chai/>
+    </>
+  )
+}
+export default App
---
+++ 01_viteBasic/src/Chai.jsx
@@ -0,0 +1,8 @@
+function Chai(){
+    return (
+        <>
+        <h1>your chai</h1>
+        </>
+    )
+}
+export default Chai
